d4a1e07f1f761893d24ab1cbb97bd6f5f80e7f1b
Андрианов Павел <andrianovpa@gmail.com>
2024-07-22 09:33:26 +0300

    Добавление теста Exception для Lion

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -6,6 +6,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest  {
 @Mock
@@ -21,6 +23,14 @@ Feline feline;
         Lion lion = new Lion("Самец", feline);
         lion.getFood();
         Mockito.verify(feline).getFood(Mockito.anyString());
+    }
+    @Test
+    public void testLionDoesHaveManeException() {
+        try {
+            Lion lion = new Lion("Используйте допустимые значения пола животного - самец или самка", feline);
+        } catch (Exception e) {
+            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
+        }
     }
 
 
